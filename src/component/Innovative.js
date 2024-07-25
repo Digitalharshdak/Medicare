a8ce112c30463602877103d3551f746478b673cf
digitalharshdak@gmail.com <digitalharshdak@gmail.com>
2024-07-26 02:11:03 +0530

    new

--- src/component/Innovative.js
+++ src/component/Innovative.js
@@ -7,32 +7,33 @@ function Innovative() {
   return (
     <div>
       <div className="innovative-container">
-      <div className="innovative-title">
-        <h1>Clinic With Innovative</h1>
-        <p>We provide the most full medical services, so every person could <br /> heave the opportunity to receive qualitative medical help.</p>
-        <button>Learn More</button>
+        <div className="innovative-title">
+          <h2>Clinic With Innovative</h2>
+          <p>We provide the most full medical services, so every person could heave the opportunity to receive qualitative medical help.</p>
+          <button>Learn More</button>
+        </div>
+
+        <div className="innovative-services">
+          <div className="innovative-service-card">
+            <img src={doctor} alt="Doctor" /> 
+            <h5>Qualified Doctors</h5>
+          </div>
+          <br/>
+          <br/>
+          <br/>
+          <div className="innovative-service-card">
+            <img src={doctors} alt="Ambulance" />
+            <h5>24 Hours Service</h5>
+          </div>
+        </div>
+
+        <div className="innovative-look">
+          <div className="innovative-service-card">
+            <img src={docto} alt="Nurse" /> 
+            <h6>Emergency Care</h6>
+          </div>  
+        </div>
       </div>
-
-      <div className="innovative-services">
-        <div className="innovative-service-card">
-          <img src={doctor} alt="Doctor" /> 
-          <h3>Qualified Doctors</h3>
-        </div>
-
-        <div className="innovative-service-card">
-          <img src={doctors} alt="Ambulance" />
-          <h3>24 Hours Service</h3>
-        </div>
-
-
-        <div className="innovative-service-card">
-          <img src={docto} alt="Nurse" /> 
-          <h3>Emergency Care</h3>
-        </div>
-      </div>
-      <br/>
-      <br/>
-    </div>
     </div>
   )
 }
